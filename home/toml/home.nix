50a66b6962574d205664eaaac96f1603c9f24485
Tom Löffler <tom.loeffler@linuxtech-it.de>
2025-09-22 14:26:39 +0200

    removed fish from home/toml/home.nix

--- home/toml/home.nix
+++ home/toml/home.nix
@@ -23,7 +23,6 @@
   # environment.
   home.packages = with pkgs; [
     warp-terminal
-    fish
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
